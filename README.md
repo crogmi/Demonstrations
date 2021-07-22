243454f71e97b49e0d3dd1386dd0883e963ae291
crogmi <chrisogami16@gmail.com>
2021-07-22 13:21:25 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+## General Information
+
+This folder contains files and projects to walkthrough various concepts throughout my studies (e.g., testing, creating my first react-app, etc.)
+
+These files are not real projects and are only used to walkthrough examples and step-by-step procedures.
---
+++ getting-started-with-jest/__tests__/filterByTerm.spec.js
@@ -0,0 +1,24 @@
+const filterByTerm = require('../Javascript/App');
+
+describe('Filter function', () => {
+    //test stuff
+    test('it should filter by a search term link', () => {
+        const input = [
+            { id: 1, url: 'https://www.url1.dev'},
+            { id: 2, url: 'https://www.url2.dev'},
+            { id: 3, url: 'https://www.link3.dev'},
+        ];
+
+        const output = [{ id: 3, url: 'https://www.link3.dev' }];
+
+        expect(filterByTerm(input, 'link')).toEqual(output);
+        expect(filterByTerm(input, 'LINK')).toEqual(output);
+        expect(filterByTerm(input, 'uRl')).toEqual([{ id: 1, url: 'https://www.url1.dev'}, { id: 2, url: 'https://www.url2.dev'}]);
+    });
+    test('it should throw when searchTerm is empty string', () => {
+        const input = [];
+        expect(() => {
+            filterByTerm(input, '');
+        }).toThrowError(Error('searchTerm cannot be empty'));
+    });
+});
